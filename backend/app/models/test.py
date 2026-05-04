9ba066add485ea2906bc00f150c79c1992ba01e7
Tay-spbu <tegodrom@gmail.com>
2026-05-04 15:14:03 +0300

    ADD: initial settings

---
+++ backend/app/models/test.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from app.core.database import Base
+
+class Test(Base):
+    __tablename__ = "test"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
