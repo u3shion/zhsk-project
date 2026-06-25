34f968ae33370d04d101e5711f9ca453fc4b26b2
u3shion <tegodrom@gmail.com>
2026-06-26 02:25:21 +0300

    ADD: initial news page

---
+++ frontend/src/pages/DashboardPage.tsx
@@ -0,0 +1,10 @@
+import './DashboardPage.css'
+
+export default function DashboardPage() {
+  return (
+    <div className="dashboard-placeholder">
+      <h1>Добро пожаловать!</h1>
+      <p>Это главная страница ЖСК-32. Контент скоро появится.</p>
+    </div>
+  )
+}
